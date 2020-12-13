ecef05f858c4bb80dcddff6ba83bb47255045290
SamuelXSS <samuelxses@unipam.edu.br>
2020-12-13 10:47:23 -0300

    backend: code organization

--- backend/src/controllers/CommentController.js
+++ backend/src/controllers/CommentController.js
@@ -3,8 +3,9 @@ const { IamTokenManager } = require("ibm-watson/auth");
 require("dotenv").config();
 
 module.exports = {
-  async find(req, res) {
+  async show(req, res) {
     const { comment_id } = req.params;
+    
     const comment = await Comment.findByPk(comment_id);
 
     return res.json(comment);
@@ -22,22 +23,14 @@ module.exports = {
     if (comment == "") {
       return res.status(400).json({ error: "Digite um comentário" });
     }
+
     if (comment.length < 5) {
       return res.status(400).json({ error: "Digite pelo menos 5 letras para comentar!" });
     }
 
-    const create = await Comment.create({
-      id,
-      comment,
-      color1,
-      color2,
-      font_color,
-    });
+    const create = await Comment.create({ id,comment,color1,color2,font_color });
 
-    return res.status(201).json({
-      success: "Comentário adicionado com sucesso, ouça ele!",
-      create,
-    });
+    return res.status(201).json({ success: "Comentário adicionado com sucesso, ouça ele!", create });
   },
 
   async speak(req, res) {
@@ -58,22 +51,20 @@ module.exports = {
   async update(req, res) {
     const { comment_id } = req.params;
     const { comment, color1, color2, font_color } = req.body;
-
+    
+    const commented = await Comment.findByPk(comment_id);
+    
     if (comment == "") {
       return res.status(400).json({ error: "Digite um comentário" });
     }
-    
-    const commented = await Comment.findByPk(comment_id);
 
     if (!commented) {
       return res.status(404).json({ error: "Esse comentário não existe :(" });
     }
 
-    const update = await commented.update({ comment, color1, color2, font_color });
+    const update = await commented.update({ comment,color1,color2,font_color });
 
-    return res
-      .status(200)
-      .json({ success: "Comentário atualizado com sucesso, ouça :D", update });
+    return res.status(200).json({ success: "Comentário atualizado com sucesso, ouça :D", update });
   },
 
   async destroy(req, res) {
@@ -83,14 +74,15 @@ module.exports = {
     const comments = await Comment.findAll();
 
     if (!comment) {
-        return res.status(404).json({ error: "Esse comentário não existe :(" });
+      return res.status(404).json({ error: "Esse comentário não existe :(" });
     }
+
     await comment.destroy({ where: { id: comment_id } });
+
     if (comments.length == 1) {
       await Comment.truncate();
     }
-    return res
-      .status(200)
-      .json({ success: "Comentário deletado com sucesso!" });
+
+    return res.status(200).json({ success: "Comentário deletado com sucesso!" });
   },
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,7 @@ const CommentController = require('./controllers/CommentController')
 const routes = express.Router()
 
 routes.get('/comments', CommentController.index)
-routes.get('/comment/:comment_id', CommentController.find)
+routes.get('/comment/:comment_id', CommentController.show)
 routes.get('/comments/speak', CommentController.speak)
 routes.post('/comments', CommentController.store)
 routes.put('/comments/:comment_id', CommentController.update)
